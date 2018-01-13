4c3845aac635ae37b484eb4630c1eadb63e9866a
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2018-01-13 14:17:47 +0100

    Remove gprolog wrapper

--- file-expert.pl
+++ file-expert.pl
@@ -1,3 +1,4 @@
+:- ['github-extensions-kb'].
 
 bang('#!').
 bang('#! ').
@@ -5,6 +6,19 @@ executablePath('/bin').
 executablePath('/usr/bin').
 executablePath('/usr/bin/env ').
 
+fileExtension(PATH, EXT):-
+    file_base_name(PATH, NAME),
+    split_string(NAME, ".", "", LIST),
+    reverse(LIST, [EXT_STR|_]),
+    not(atom_string(NAME, EXT_STR)),
+    atom_concat('.', EXT_STR, EXT).
+
+
+fileFirstLine(PATH, FIRST_LINE):-
+    exists_file(PATH),
+    open(PATH, read, Stream),
+    read_line_to_codes(Stream, Codes),
+    atom_chars(FIRST_LINE, Codes).
 
 shebangType(PATH, TYPE):-
     fileFirstLine(PATH, MagicLine),
