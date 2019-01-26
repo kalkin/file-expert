0c8a197846286fb90b94cdea001480b9f372317f
Bahtiar Gadimov <bahtiar@dia-com.de>
2019-01-26 22:17:07 +0100

    Recognize ShaderLab .shader files

--- extra-extensions-kb.pl
+++ extra-extensions-kb.pl
@@ -19,4 +19,5 @@ file_extension('.zunit', 'Shell').
 file_extension('.cfg', 'HAProxy', '^(global|default|frontend|backend)\\s*\n\\s+').
 file_extension('.inc', 'Pascal', "^\\s*end[.;]").
 file_extension('.re', 'Reason', '\\blet\\b').
+file_extension('.shader', 'ShaderLab', '^\\s*Shader\s+".*"$').
 file_extension('.v', 'Verilog', '^\\s*endmodule\\s*$').
