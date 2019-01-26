9eeef5799a7fa6d6822bef04e04338fd8f9a903e
Bahtiar Gadimov <bahtiar@dia-com.de>
2019-01-26 22:42:17 +0100

    Recognize XML .pluginspec files

--- extra-extensions-kb.pl
+++ extra-extensions-kb.pl
@@ -21,5 +21,6 @@ file_extension('.inc', 'Pascal', "^\\s*end[.;]").
 file_extension('.fcgi', 'PHP', "<\\?php").
 file_extension('.mod', 'Linux Kernel Module', "^[a-zA-Z0-9/_-]+\\.(ko|o)$").
 file_extension('.re', 'Reason', '\\blet\\b').
+file_extension('.pluginspec', 'XML', '<\\\\\\w+>').
 file_extension('.shader', 'ShaderLab', '^\\s*Shader\s+".*"$').
 file_extension('.v', 'Verilog', '^\\s*endmodule\\s*$').
