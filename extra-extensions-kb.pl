b78f9d4581dca4d4642de05475efaa8aa781c647
Bahtiar Gadimov <bahtiar@dia-com.de>
2019-01-26 22:06:18 +0100

    Recognize Reason .re files

--- extra-extensions-kb.pl
+++ extra-extensions-kb.pl
@@ -18,4 +18,5 @@ file_extension('WOFF', '.woff').
 file_extension('.zunit', 'Shell').
 file_extension('.cfg', 'HAProxy', '^(global|default|frontend|backend)\\s*\n\\s+').
 file_extension('.inc', 'Pascal', "^\\s*end[.;]").
+file_extension('.re', 'Reason', '\\blet\\b').
 file_extension('.v', 'Verilog', '^\\s*endmodule\\s*$').
