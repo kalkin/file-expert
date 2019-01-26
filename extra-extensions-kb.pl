6b973d49c8f0859ac4f3ddc157027f438b6f1c3c
Bahtiar Gadimov <bahtiar@dia-com.de>
2019-01-26 22:28:07 +0100

    Recognize .fcgi PHP files

--- extra-extensions-kb.pl
+++ extra-extensions-kb.pl
@@ -18,6 +18,7 @@ file_extension('WOFF', '.woff').
 file_extension('.zunit', 'Shell').
 file_extension('.cfg', 'HAProxy', '^(global|default|frontend|backend)\\s*\n\\s+').
 file_extension('.inc', 'Pascal', "^\\s*end[.;]").
+file_extension('.fcgi', 'PHP', "<\\?php").
 file_extension('.re', 'Reason', '\\blet\\b').
 file_extension('.shader', 'ShaderLab', '^\\s*Shader\s+".*"$').
 file_extension('.v', 'Verilog', '^\\s*endmodule\\s*$').
