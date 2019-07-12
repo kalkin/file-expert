e85e683068e67c2a6455af40c49f1b42fdaef7d8
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2019-07-12 20:21:45 +0200

    Add AGPL header to source files

--- lib/file.pl
+++ lib/file.pl
@@ -1,3 +1,20 @@
+%  Copyright © 2019 Bahtiar `kalkin` Gadimov
+%
+%    This file is part of file-expert.
+%
+%  file-expert is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU Affero General Public License as published by
+%  the Free Software Foundation, either version 3 of the License, or
+%  (at your option) any later version.
+%
+%  file-expert is distributed in the hope that it will be useful,
+%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU Affero General Public License for more details.
+%
+%  You should have received a copy of the GNU Affero General Public License
+%  along with file-expert.  If not, see <https://www.gnu.org/licenses/>.
+
 :- module(file, [parse_extension/2, read_file/3, first_line/2,
     list_files_recursive/2]).
 
