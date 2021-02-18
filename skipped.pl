abed468176ca8d6b697ba561ae89db88e6cd7f4e
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2021-02-18 23:24:13 +0100

    test: Enable some previously skipped tests

--- skipped.pl
+++ skipped.pl
@@ -1,15 +1,7 @@
 skipped('linguist/samples/Altium Designer/Sample Altium Project.PrjPcb').
 skipped('linguist/samples/Fstar/Hacl.HKDF.fst').
 skipped('linguist/samples/Fstar/Hacl.Spec.Bignum.Fmul.fst').
-skipped('linguist/samples/Java Properties/libraries.properties').
-skipped('linguist/samples/Java Properties/sounds.properties').
-skipped('linguist/samples/Linker Script/inject.x').
 skipped('linguist/samples/Python/python').
 skipped('linguist/samples/Ruby/ruby2').
 skipped('linguist/samples/Scala/99-bottles-of-beer').
 skipped('linguist/samples/Scala/scala').
-skipped('linguist/samples/TSX/import.tsx').
-skipped('linguist/samples/TSX/react-native.tsx').
-skipped('linguist/samples/TSX/require.tsx').
-skipped('linguist/samples/TSX/triple-slash-reference.tsx').
-skipped('linguist/samples/X PixMap/cc-public_domain_mark_white.pm').
