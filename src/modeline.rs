98462098f100589698f72535177c2003c383cf74
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2021-06-12 19:45:31 +0200

    feat: Parse Emacs modeline

--- src/modeline.rs
+++ src/modeline.rs
@@ -3,11 +3,17 @@ use lazy_static::lazy_static;
 
 lazy_static! {
     static ref VI_REGEX: Regex =
-        Regex::new(r#"\bvim?\b.*\b(?:filetype|ft)=(.+?)\b"#).expect("Valid Regex");
+        Regex::new(r#"\bvim?\b.*\b(?:filetype|ft|syntax|syn)=(.+?)\b"#).expect("Valid Regex");
+    static ref EMACS_REGEX: Regex =
+        Regex::new(r#"\s-\*-\s+(?:mode:\s+)?(\w+)\b.*\s-\*-"#).expect("Valid Regex");
 }
 
 pub fn parse_modeline(line: &str) -> Option<&str> {
     if let Some(caps) = VI_REGEX.captures(line).unwrap() {
+        if let Some(m) = caps.get(1) {
+            return Some(m.as_str());
+        }
+    } else if let Some(caps) = EMACS_REGEX.captures(line).unwrap() {
         if let Some(m) = caps.get(1) {
             return Some(m.as_str());
         }
@@ -16,7 +22,7 @@ pub fn parse_modeline(line: &str) -> Option<&str> {
 }
 
 #[test]
-fn test() {
+fn vim_modeline() {
     let expected = Some("perl6");
 
     let actual = parse_modeline("# vim: set ft=perl6");
@@ -41,5 +47,16 @@ fn test() {
     assert_eq!(expected, actual);
 
     let actual = parse_modeline(":vim:tw=78:ts=8:ft=perl6:norl:fen:fdl=0:fdm=marker:");
+    assert_eq!(expected, actual);
+}
+
+#[test]
+fn emacs_modeline() {
+    let expected = Some("Lisp");
+    let actual = parse_modeline("# -*- Lisp -*-");
+    assert_eq!(expected, actual);
+
+    let expected = Some("Lisp");
+    let actual = parse_modeline("# -*- mode: Lisp; eval: (auto-fill-mode 1); -*-");
     assert_eq!(expected, actual);
 }
