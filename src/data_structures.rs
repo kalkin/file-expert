ab1e19d21fcb6a8179cd38e94afa54087ff6f28e
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2021-06-13 17:33:35 +0200

    style: Make clippy ♥

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -18,8 +18,9 @@ impl Content {
     pub fn new(path: &Path) -> Result<Self, std::io::Error> {
         let mut file = File::open(path)?;
         let file_size = file.metadata()?.len();
+        #[allow(clippy::cast_possible_truncation)]
         let amount_to_read = if file_size < MAX_SIZE as u64 {
-            file_size as usize
+            file_size as usize // file_size is already < MAX_SIZE which fits in a `usize`
         } else {
             MAX_SIZE
         };
@@ -36,9 +37,9 @@ impl Content {
                 .map(String::from)
                 .collect::<Vec<String>>();
             let mut modelines: Vec<String> = Vec::with_capacity(5);
-            modelines.append(&mut body.iter().take(5).map(|s| s.to_string()).collect());
+            modelines.append(&mut body.iter().take(5).map(ToString::to_string).collect());
             if amount_to_read < MAX_SIZE {
-                modelines.append(&mut body.iter().rev().take(5).map(|s| s.to_string()).collect())
+                modelines.append(&mut body.iter().rev().take(5).map(ToString::to_string).collect())
             } else {
                 file.seek(SeekFrom::End(-4096))?;
                 let mut tmp = vec![0_u8; 4096];
@@ -47,7 +48,7 @@ impl Content {
                     .lines()
                     .rev()
                     .take(5)
-                    .map(|s| s.to_string())
+                    .map(ToString::to_string)
                     .collect::<Vec<_>>();
                 modelines.append(&mut last_5_lines)
             }
@@ -56,7 +57,7 @@ impl Content {
         }
     }
 
-    fn is_binary(data: &Vec<u8>) -> bool {
+    fn is_binary(data: &[u8]) -> bool {
         return data.iter().any(|i| i == &0_u8);
     }
 }
