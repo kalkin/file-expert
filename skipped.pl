a50acbdd3fd497f396acd5f12ce17681032f15ca
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2021-08-30 21:28:52 +0200

    Remove old Prolog code

--- skipped.pl
+++
@@ -1,7 +0,0 @@
-skipped('linguist/samples/Altium Designer/Sample Altium Project.PrjPcb').
-skipped('linguist/samples/Fstar/Hacl.HKDF.fst').
-skipped('linguist/samples/Fstar/Hacl.Spec.Bignum.Fmul.fst').
-skipped('linguist/samples/Python/python').
-skipped('linguist/samples/Ruby/ruby2').
-skipped('linguist/samples/Scala/99-bottles-of-beer').
-skipped('linguist/samples/Scala/scala').
