e7f41d7fcdb5b660b89d8c023fd3ce7e45b82c7f
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2021-09-04 22:03:35 +0200

    style: Add AGPL header

--- src/linguist_heuristics.rs
+++ src/linguist_heuristics.rs
@@ -1,3 +1,22 @@
+//
+// Copyright (c) 2018-2020 Bahtiar `kalkin-` Gadimov.
+//
+// This file is part of file-expert
+// (see https://github.com/kalkin/file-expert).
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
 use fancy_regex::Regex;
 use lazy_static::lazy_static;
 
